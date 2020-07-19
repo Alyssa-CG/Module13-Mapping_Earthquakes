11c35244e66f099b932c4223aabff9c751256ec0
Alyssa Gomes <alyssa@Alyssas-MacBook-Air.local>
2020-07-19 02:47:33 -0400

    Steps 1 and 2 in mapping Earthquake data done

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -38,7 +38,35 @@ let earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all
 
 // Grabbing our GeoJSON data.
 d3.json(earthquakes).then(function(data) {
+  console.log(data);
+    // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into a function
+    // to calculate the radius.
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(),
+        stroke: true,
+        weight: 0.5
+      };
+    }
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+      if (magnitude === 0) {
+        return 1;
+      }
+      return magnitude * 4;
+    }
+  // Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  pointToLayer: function(feature, latlng) {
     console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+    return L.circleMarker(latlng);
+  },
+  style: styleInfo,
+}).addTo(map);
 });
