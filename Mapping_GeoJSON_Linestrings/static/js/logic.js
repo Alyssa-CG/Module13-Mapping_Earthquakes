11eba881e8d8fc27f5eb17b8659881ba04e57406
Alyssa Gomes <alyssa@Alyssas-MacBook-Air.local>
2020-07-19 00:21:52 -0400

    progress

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -18,7 +18,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
@@ -26,7 +26,7 @@ let baseMaps = {
 let map = L.map('mapid', {
 	center: [30, 30],
 	zoom: 2,
-	layers: [streets]
+	layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -40,7 +40,10 @@ let airportData = "https://raw.githubusercontent.com/Alyssa-CG/Module13-Mapping_
 d3.json(airportData).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data)
-  // .bindPopup(data.features.properties.name)
+  L.geoJson(data, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(feature.properties.faa)
+    }
+  })
   .addTo(map);
 });
